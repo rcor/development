7d5e70e06b89c0425780b5b27bcdd2385f01ad4f
Rodolfo Cordero <rcor.cr@gmail.com>
2022-03-20 23:45:58 -0300

    Add Run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-run -p 3000:3000 rcor/term -d 
+if [ "$( docker container inspect -f '{{.State.Status}}' development )" == "exited" ];
+then
+    docker start development
+else 
+    if [ "$( docker container inspect -f '{{.State.Running}}' development )" == "true" ];
+    then
+        echo "It is running"
+    else 
+        docker run -d -p 3000:3000 --name development rcor/term 
+    fi 
+fi 
+
+
+#Only on MAC 
+open -a "Google Chrome" http://localhost:3000/wetty/
